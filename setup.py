ac2d36b6d8a02683e3172e43327645f03aecd849
prateekta <prateek.agarwal@tigeranalytics.com>
2020-06-28 18:31:14 +0530

    added setup.py and relevant files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="housing_predicion",
+    author="Prateek",
+    author_email="prateek.agarwal@tigeranalytics.com",
+    version="0.1",
+    packages=find_packages(),
+    description="Train and predict multiple models on housing prediction",
+)
